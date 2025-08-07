5d98792f42bee41e26b7902a6b147993736f5870
Deniz <auo@tuta.io>
2025-08-07 10:46:15 +0200

    added follow button to the Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,6 +3,7 @@ function Header() {
           <header>
                <h1>NeiDan Music Production</h1>
                <p>Chilled Psy Prog</p>
+               <iframe scrolling="no" src="https://bandcamp.com/band_follow_button_classic/2606447443"></iframe>
           </header>
      );
 }
